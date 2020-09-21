0698f98ad0559e82828d05f074720e7cbf6ae928
User <User@DESKTOP-CAEBGTR>
2020-09-21 12:26:24 +0700

    initial project

---
+++ app/Models/Page.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use App\Base\SluggableModel;
+use DateTimeInterface;
+
+class Page extends SluggableModel
+{
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parent(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(__CLASS__);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function children(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(__CLASS__, 'parent_id', 'id');
+    }
+
+    /**
+     * @return string
+     */
+    public function getLinkAttribute(): string
+    {
+        return route('page', ['pSlug' => $this->slug]);
+    }
+
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+}
---
+++ database/migrations/2015_04_06_131951_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug');
+            $table->string('title');
+            $table->string('description');
+            $table->timestamps();
+            $table->index('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
