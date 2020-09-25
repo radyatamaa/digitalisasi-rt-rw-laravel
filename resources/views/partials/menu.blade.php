ff1c02f070c799f106e6233c0da29f2ace6ee34b
User <User@DESKTOP-CAEBGTR>
2020-09-26 02:14:08 +0700

    Revert "push master crud gaji, job, agama"
    This reverts commit eb55061f64bb17678447a5ec46841303dbb6b906.

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -62,28 +62,29 @@
             </li>
 
             <li class="nav-item">
-                <a href="{{ route("admin.master_agama.index") }}" class="nav-link {{ request()->is('admin/master_agama') || request()->is('admin/master_agama/*') ? 'active' : '' }}">
+                <a href="{{ route("admin.event_category.index") }}" class="nav-link {{ request()->is('admin/event_category') || request()->is('admin/event_category/*') ? 'active' : '' }}">
                     <i class="fas fa-cogs nav-icon">
 
                     </i>
-                    {{ trans('global.master_agama.title') }}
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ route("admin.master_pekerjaan.index") }}" class="nav-link {{ request()->is('admin/master_pekerjaan') || request()->is('admin/master_pekerjaan/*') ? 'active' : '' }}">
-                    <i class="fas fa-cogs nav-icon">
-
-                    </i>
-                    {{ trans('global.master_pekerjaan.title') }}
+                    {{ trans('global.event_category.title') }}
                 </a>
             </li>
 
             <li class="nav-item">
-                <a href="{{ route("admin.master_gaji.index") }}" class="nav-link {{ request()->is('admin/master_gaji') || request()->is('admin/master_gaji/*') ? 'active' : '' }}">
+                <a href="{{ route("admin.insidental_category.index") }}" class="nav-link {{ request()->is('admin/insidental_category') || request()->is('admin/insidental_category/*') ? 'active' : '' }}">
                     <i class="fas fa-cogs nav-icon">
 
                     </i>
-                    {{ trans('global.master_gaji.title') }}
+                    {{ trans('global.insidental_category.title') }}
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="{{ route("admin.history_category.index") }}" class="nav-link {{ request()->is('admin/history_category') || request()->is('admin/history_category/*') ? 'active' : '' }}">
+                    <i class="fas fa-cogs nav-icon">
+
+                    </i>
+                    {{ trans('global.history_category.title') }}
                 </a>
             </li>
 
