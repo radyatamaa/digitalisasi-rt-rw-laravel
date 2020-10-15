04bf2a832013a1ae43eea00aa50f47095004f4bd
Ferdian Nur <68576009+ferdiannur11@users.noreply.github.com>
2020-10-16 00:18:06 +0700

    fixing sdm

--- resources/views/admin/sdm/index.blade.php
+++ resources/views/admin/sdm/index.blade.php
@@ -465,12 +465,12 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>List Kategori SDM</h1>
+                            <h1>List SDM</h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">List Kategori SDM</li>
+                                <li class="breadcrumb-item active">List SDM</li>
                             </ol>
                         </div>
                     </div>
