5218cfc67af3abf0aa6ca13bed1c854f031c647e
User <User@DESKTOP-CAEBGTR>
2020-10-28 22:34:08 +0700

    fiixing redirect home all page

--- resources/views/admin/pendidikan/index.blade.php
+++ resources/views/admin/pendidikan/index.blade.php
@@ -31,7 +31,7 @@
           <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-          <a href="#" class="nav-link">Home</a>
+          <a href="{{ route("admin.index") }}" class="nav-link">Home</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
           <a href="#" class="nav-link">Contact</a>
@@ -475,7 +475,7 @@
             </div>
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{ route("admin.index") }}">Home</a></li>
                 <li class="breadcrumb-item active">List Pendidikan</li>
               </ol>
             </div>
