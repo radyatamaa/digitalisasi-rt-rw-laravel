52a664e884eaf49f803041ecd651982315a972dd
bagusshndr <57434480+bagusshndr@users.noreply.github.com>
2020-12-28 17:26:41 +0700

    PUSH

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,11 +47,11 @@
                                     <input name="remember" type="checkbox" /> {{ trans('global.remember_me') }}
                                 </label>
                             </div>
-                            <div class="col-6 text-right" >
+                            <!-- <div class="col-6 text-right" >
                                 <a class="btn btn-link px-0" href="{{ route('password.request') }}" style="color: black;">
                                     {{ trans('global.forgot_password') }}
                                 </a>
-                            </div>
+                            </div> -->
                         </div>
                     </form>
                 </div>
